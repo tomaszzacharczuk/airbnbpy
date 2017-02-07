6e9c2837863ac191b8c94c6c0237de35ad522d33
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-02-07 11:16:00 +0100

    Create public endpoints

---
+++ airbnbpy/core.py
@@ -0,0 +1,179 @@
+import requests
+from fake_useragent import UserAgent
+from fake_useragent import FakeUserAgentError
+
+
+class AuthError(BaseException):
+    pass
+
+
+class AirbnbAPI(object):
+    """SDK for Airbnb API
+
+    Based on documentation from: http://airbnbapi.org/
+    """
+
+    API_URL = "https://api.airbnb.com"
+
+    def __init__(self, client_id: str):
+        """
+        :param client_id: (API Key)
+        """
+        self.client_id = client_id
+        self._access_token = None
+        self._response = None
+        self._session = requests.Session()
+
+    def login_by_google(self):
+        """TODO: I don't need it but perhaps someday I'll implement it"""
+        raise NotImplementedError
+
+    def login_by_facebook(self):
+        """TODO: I don't need it but perhaps someday I'll implement it"""
+        raise NotImplementedError
+
+    def login_by_email(self, username: str, password: str):
+        """Gets an access_token, given a valid user account email and password.
+
+        :param username:        airbnbdev@gmail.com     Account's email address
+        :param password:        password                Account's clear-text password (note: endpoint uses HTTPS)
+        """
+
+        try:
+            ua = UserAgent()
+            user_agent = ua.random
+        except FakeUserAgentError:
+            user_agent = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) ' \
+                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+
+        self._session.headers.update({
+            "Accept": "application/json",
+            "Accept-Encoding": "gzip, deflate",
+            "Content-Type": "application/json",
+            'User-Agent': user_agent
+        })
+
+        login_payload = {
+            "client_id": self.client_id,
+            "grant_type": 'password',
+            "username": username,
+            "password": password,
+            "prevent_account_creation": "true"
+        }
+
+        self._response = self._session.post(self.API_URL + "/v1/authorize", json=login_payload)
+
+        try:
+            self._access_token = self._response.json()['access_token']
+            self._session.headers.update({"X-Airbnb-OAuth-Token": self._access_token})
+        except KeyError:
+            raise AuthError('{} - {}'.format(
+                self._response.json()['error_code'], self._response.json()['error_message']))
+
+    def listing_search(self, **optional_params) -> requests.models.Response:
+        """
+        Optional URL Parameters:
+
+        Key	                    Sample Value	                            Description
+        locale	                en-US	                                    Desired lagnuage
+        currency	            USD	                                        Desired currency
+        _format	                for_search_results
+                                || for_search_results_with_minimal_pricing	Search with pricing or not.
+        _limit	                10	                                        Number of listings to show at a time.
+        _offset	                0	                                        Number of listings to offset in search.
+        guests	                1	                                        Number of guests.
+        ib	                    false	                                    Setting to true will only show listings
+                                                                            that are instant bookable.
+        ib_add_photo_flow	    true	                                    Not sure.
+        location	            Los%20Angeles%2C%20CA	                    Search by location name
+                                                                            -- if unsure of lat/lng, etc.
+        min_bathrooms	        0	                                        Minimum number of bathrooms.
+        min_bedrooms	        0	                                        Minimum number of bedrooms.
+        min_beds	            0	                                        Minimum number of beds.
+        price_min	            40	                                        Minimum price.
+        price_max	            210	                                        Maximum price.
+        min_num_pic_urls	    10	                                        Minimum number of pictures.
+        sort	                1	                                        Sorting order
+                                                                            (1: forward order, 0: reverse order).
+        suppress_facets	        true	                                    Not sure.
+        user_lat	            37.18722222222222	                        Latitude search coordinate.
+        user_lng	            -122.42833333333333	                        Longitude search coordinate.
+
+        """
+        return self._session.get(self.API_URL + "/v2/search_results/", data=optional_params)
+
+    def view_listing_info(self, listing_id, **optional_params) -> requests.models.Response:
+        """Returns detailed information about a listing, given its ID (e.g., found in the search endpoint reponse).
+
+        Optional URL Parameters:
+
+        Key                 Sample Value	    Description
+        locale	            en-US	            Desired lagnuage
+        _source	            mobile_p3	        Not sure. I'm guessing this means the request is coming from
+                                                an Android mobile phone.
+        number_of_guests	1	                Determines listing availability dates based on the # of guests.
+
+
+        :param listing_id:
+        :return requests.models.Response
+        """
+
+        # API result format(just put this - it won't work without it)
+        optional_params.update({"_format": "v1_legacy_for_p3"})
+        return self._session.get(self.API_URL + "/v2/listings/" + listing_id, data=optional_params)
+
+    def get_reviews(self, listing_id: str, **optional_params) -> requests.models.Response:
+        """Returns reviews for a given listing.
+
+
+        Optional URL Parameters:
+
+        Key	        Sample Value	                                    Description
+        locale	    en-US	                                            Desired lagnuage
+        currency	USD	                                                Desired currency
+        _format	    for_mobile_client
+                    || for_search_results
+                    || for_search_results_with_minimal_pricing	        Not sure what the difference is.
+        _limit	    10	                                                Number of reviews to show at a time.
+        _offset	    0	                                                Number of reviews to offset.
+
+        :param listing_id:
+        :param optional_params:
+        :return requests.models.Response:
+        """
+        optional_params.update({"listing_id": listing_id, "role": "all"})
+        return self._session.get(self.API_URL + "/v2/reviews/", data=optional_params)
+
+    def view_user_info(self, user_id: int, **optional_params) -> requests.models.Response:
+        """Returns detailed information about a user, given his/her/its ID
+        (e.g., found in the view listing endpoint response).
+
+        Optional URL Parameters:
+
+        Key	        Sample Value	    Description
+        locale	    en-US	            Desired lagnuage
+        currency	USD	                Currency for listings.
+        """
+        optional_params.update({"user_id": user_id, "_format": "v1_legacy_show"})
+        return self._session.get(self.API_URL + "/v2/users", data=optional_params)
+
+    def get_host_listings(self, user_id: int, **optional_params) -> requests.models.Response:
+        """Returns information about all the listings a user hosts.
+
+        Optional URL Parameters:
+
+        Key	                    Sample Value	        Description
+        locale	                en-US	                Desired lagnuage
+        currency	            USD	                    Desired currency
+        _format	                v1_legacy_long	        Not sure...
+        _limit	                10	                    Max listings to return
+        _offset	                0	                    Listing offset
+        has_availability	    false	                Whether to show listings that are currently active or not
+        user_id	                57297136	            The ID of the user whose listings you'd like to get
+        """
+        optional_params.update({"user_id": user_id})
+        return self._session.get(self.API_URL + "/v2/listings", data=optional_params)
+
+
+if __name__ == '__main__':
+    pass
